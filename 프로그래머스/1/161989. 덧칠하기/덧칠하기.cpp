55af393be594b996966204c444b2216559f075e5
Moon Junsu <105295416+junsu1211@users.noreply.github.com>
2025-01-22 12:10:58 +0900

    [level 1] Title: 덧칠하기, Time: 3.26 ms, Memory: 4.73 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -5,27 +5,30 @@ using namespace std;
 
 int solution(int n, int m, vector<int> section) {
     int answer = 0;
-    
-    int* wall = new int[n+1];
-    for(int i=1; i<=n; i++)
-        wall[i] = 1;
-    
-    
-    // O(n) 은 안됨
-    for(int i=0; i<section.size(); i++){
-        wall[ section[i] ] = 0; // 페인트가 벗겨짐
-    }
-    
+    // n: (1미터인)구역의 개수, m: 롤러의 크기
     int count=0;
-    for(int i=1; i<=n; i++){
-        
-        if(wall[ i ] == 0){ // 페인트가 벗겨진 곳이면
-            i = i + m - 1;
-            count++;
+    int* dp = new int[n+1];
+    // 배열
+    int idx=0;
+    int i=1;
+    int end=0;
+    
+    for(i=1; i<=n;i++){ //페인트칠을 시작하는 것이 가능한 위치의 범위
+        if(section[idx] == i && section[idx] > end){
+            end = i+m-1; // 마지막까지 칠해진 곳의 위치 
+            printf("%d\n", i);
+            answer++;
+            idx++;
         }
-        
+        else if(section[idx] == i && section[idx] <= end){
+            idx++;
+        }
     }
-    answer = count;
+    
+    
+    
+    
+    
     
     return answer;
 }
